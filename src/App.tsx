8db5c1aa075c558f9d7e6fd6d5c9389b83949403
Maori Benhassine <maori.benhassine@gmail.com>
2025-07-23 14:37:38 +0200

    Enable HashRouting instead of BrowserRouting for single file serving

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { logger } from "./utils/logger";
 import Index from "./pages/Index";
@@ -45,7 +45,7 @@ const App = () => (
             <TooltipProvider>
                 <Toaster />
                 <Sonner />
-                <BrowserRouter>
+                <HashRouter>
                     <ErrorBoundary>
                         <Routes>
                             <Route path="/" element={<Index />} />
@@ -54,7 +54,7 @@ const App = () => (
                             <Route path="*" element={<NotFound />} />
                         </Routes>
                     </ErrorBoundary>
-                </BrowserRouter>
+                </HashRouter>
             </TooltipProvider>
         </QueryClientProvider>
     </ErrorBoundary>
